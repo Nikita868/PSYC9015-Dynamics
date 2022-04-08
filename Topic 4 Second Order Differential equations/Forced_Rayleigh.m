de1d612dd56a40edb7e527c8699cf5a4603773ec
Nikita Kuznetsov <72103196+Nikita868@users.noreply.github.com>
2022-04-08 12:39:20 -0400

    Add files via upload

--- Topic 4 Second Order Differential equations/Forced_Rayleigh.m
+++ Topic 4 Second Order Differential equations/Forced_Rayleigh.m
@@ -1,9 +1,9 @@
-%[t,x] = Forced_Ray_do(Pos, K, sigma, beta, fw, stoptime, plotflag);
+%[t,x] = Forced_Ray_do(Pos, K, epsilon, beta, fw, stoptime, plotflag);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % M.J.Richardson 1/2007
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [t,x] = Forced_Rayleigh(Pos, K, sigma, beta, fw, stoptime, plotflag)
+function [t,x] = Forced_Rayleigh(Pos, k, epsilon, beta, fw, stoptime, plotflag)
 
 %    Examples
 %   [t,x] = Forced_Rayleigh(.2, 1.8*pi, 1, 1, 2*pi, 30, 1);
@@ -27,7 +27,7 @@ y0 = [Pos 0 1]';
 fprintf('processing... ... ...\n');
 %Integrate:
 options = odeset('InitialStep',H_INC,'MaxStep',H_INC,'RelTol',realmax,'AbsTol',[realmax realmax realmax]);
-[t,x]=ode45(@Forced_Ray_Sim,tspan,y0,options,K,sigma,beta,fw);
+[t,x]=ode45(@Forced_Ray_Sim,tspan,y0,options,k,epsilon,beta,fw);
 
 close all;
 if (plotflag == 1)
@@ -64,12 +64,12 @@ return;
 % M.J.Richardson 9/2007
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function dxdt = Forced_Ray_Sim(t,y,k,sigma,beta,fw)
+function dxdt = Forced_Ray_Sim(t,y,k,epsilon,beta,fw)
 
 %One line for each state:
 dxdt =[ 
        y(2)
-       -k^2*y(1)-sigma*(((y(2)^2)-1)*y(2))+ beta*cos(y(3)*fw)
+       -k^2*y(1)-epsilon*(((y(2)^2)-1)*y(2))+ beta*cos(y(3)*fw)
        1
 ];
 
